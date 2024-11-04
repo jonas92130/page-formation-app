d92bb8ea679e7c380b17ad8879f1288d5a21d477
Yousra Belbaz <yousra.belbaz@epitech.eu>
2024-11-04 15:00:28 +0100

    FEAT : adding the bg image

--- app/clientPage.tsx
+++ app/clientPage.tsx
@@ -22,37 +22,39 @@ function ClientPage(props: Props) {
       <div>
         <SearchBar />
       </div>
-      <div className="mx-8 flex flex-col lg:mx-36 lg:max-w-[1400px]">
-        <div className="flex flex-col items-center justify-center gap-12 lg:gap-24 pt-16 lg:flex-row">
-          <Formation
-            title="Formations CPF"
-            label="Se former avec son compte personnel de formation"
-            backgroundColor="bg-secondary"
+      <div className='background-image'>
+        <div className="mx-8 flex flex-col lg:mx-36 lg:max-w-[1400px]">
+          <div className="flex flex-col items-center justify-center gap-12 pt-16 lg:flex-row lg:gap-24">
+            <Formation
+              title="Formations CPF"
+              label="Se former avec son compte personnel de formation"
+              backgroundColor="bg-secondary"
+            />
+            <Formation
+              title="Formations en ligne"
+              label="Se former de chez soi et à son rythme"
+              backgroundColor="bg-primary"
+              isArrow={true}
+            />
+          </div>
+          <LinkList
+            name="domain"
+            label="Quels domaines vous interesses ?"
+            backgroundColor="bg-card"
           />
-          <Formation
-            title="Formations en ligne"
-            label="Se former de chez soi et à son rythme"
-            backgroundColor="bg-primary"
-            isArrow={true}
+          <ActualityHome />
+          <LinkList
+            name="domain"
+            label="Les domaines professionnels"
+            backgroundColor="bg-primary/85 text-primary-foreground"
+          />
+          <CitiesServerList />
+          <LinkList
+            name="domain"
+            label="Les métiers populaires"
+            backgroundColor="bg-secondary/15 text-primary-background"
           />
         </div>
-        <LinkList
-          name="domain"
-          label="Quels domaines vous interesses ?"
-          backgroundColor="bg-card"
-        />
-        <ActualityHome />
-        <LinkList
-          name="domain"
-          label="Les domaines professionnels"
-          backgroundColor="bg-primary/85 text-primary-foreground"
-        />
-        <CitiesServerList />
-        <LinkList
-          name="domain"
-          label="Les métiers populaires"
-          backgroundColor="bg-secondary/15 text-primary-background"
-        />
       </div>
       <Footer />
     </main>
