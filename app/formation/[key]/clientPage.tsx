46a19840f2c6ccdf942354dc95bc805c0ee54359
Yousra Belbaz <yousra.belbaz@epitech.eu>
2024-11-06 17:39:53 +0100

    FEAT : start design of page Formation

--- app/formation/[key]/clientPage.tsx
+++ app/formation/[key]/clientPage.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Formation } from '@/model/formation'
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardDescription } from '@/components/ui/card'
+import { TiPlus } from 'react-icons/ti'
+import { TiChevronRight } from 'react-icons/ti'
+
 type Props = {
   data: Formation
 }
@@ -10,38 +13,57 @@ function ClientPage(props: Props) {
   const { data } = props
   console.log(data)
   return (
-    <>
-      <h1 className="font-bold lg:text-3xl">{data.intitule_formation}</h1>
-      <p>{data.nom_of}</p>
-      <div className="flex gap-5 py-2">
-        <Badge className="rounded-md">Eligibles CPF</Badge>
-        <Badge className="rounded-md border border-primary bg-background text-primary">
-          A distance
-        </Badge>
-      </div>
-      <Card>
-        <CardDescription>
-          {data.nombre_heures_total_max > 0
-            ? `  Durée : ${data.nombre_heures_total_max}h de formation`
-            : null}
-        </CardDescription>
-        <CardDescription>Prix : {data.frais_ttc_tot_max}€</CardDescription>
-        <CardDescription>
-          Localisation : {data.nom_departement} {data.code_departement}
-        </CardDescription>
-      </Card>
-      <Card>
-        {data.avis ? (
-          <CardContent>4,3 ⭐️⭐️⭐️⭐️⭐️ 100 avis{data.avis}</CardContent>
+    <div className="background-image">
+      <div className="mx-auto flex w-[90%] max-w-[1200px] flex-col gap-2">
+        <h1 className="pt-5 font-extrabold lg:text-3xl">
+          {data.intitule_formation}
+        </h1>
+        <p>{data.nom_of}</p>
+        <div className="flex gap-5 py-2">
+          <Badge className="rounded-md">Eligibles CPF</Badge>
+          <Badge className="rounded-md border border-primary bg-background text-primary">
+            A distance
+          </Badge>
+        </div>
+        <p className="flex flex-row items-center text-base">
+          <span className="font-extrabold">
+            <TiPlus />
+          </span>
+          Voir des formations similaires
+        </p>
+        <Card className="flex flex-col gap-2 border px-5 py-4 shadow-sm">
+          <p>
+            {data.nombre_heures_total_max > 0
+              ? `Durée : ${data.nombre_heures_total_max}h de formation`
+              : null}
+          </p>
+          <p>Prix : {data.frais_ttc_tot_max}€</p>
+          <p>
+            {data.nom_departement && data.code_departement
+              ? `Localisation : ${data.nom_departement} ${data.code_departement}`
+              : null}
+          </p>
+        </Card>
+        {data ? (
+          <Card className="bg-primary/20 px-5 py-4">
+            <p>4,3 ⭐️⭐️⭐️⭐️⭐️ 100 avis{data.avis}</p>
+          </Card>
         ) : null}
-      </Card>
-      <h4>Objectif</h4>
-      <p>{data.objectif_formation}</p>
-      <h4>Contenu</h4>
-      <p>{data.contenu_formation}</p>
-      <h4>Points Forts</h4>
-      <p>{data.points_forts}</p>
-    </>
+        <h4 className="text-lg font-bold">Objectif</h4>
+        <p>{data.objectif_formation}</p>
+        <div className="flex items-center">
+          <TiChevronRight />
+          <p className="font-bold text-sm"> Voir plus </p>
+        </div>
+
+        <h4 className="text-lg font-bold">Contenu</h4>
+        <p>{data.contenu_formation}</p>
+        <Card className="flex flex-col gap-3 bg-primary/20 px-5 py-4">
+          <h4 className="text-lg font-bold">Points Forts 🔥</h4>
+          <p className="">{data.points_forts}</p>
+        </Card>
+      </div>
+    </div>
   )
 }
 
