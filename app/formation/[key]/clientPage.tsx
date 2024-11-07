4175ab1d883331b03ca71447a26da9903a37faf1
Yousra Belbaz <yousra.belbaz@epitech.eu>
2024-11-07 11:42:16 +0100

    FEAT : modifying design of formation page

--- app/formation/[key]/clientPage.tsx
+++ app/formation/[key]/clientPage.tsx
@@ -17,21 +17,27 @@ function ClientPage(props: Props) {
   console.log(data)
   return (
     <div className="background-image">
-      <div className="mx-auto flex w-[90%] max-w-[1200px] flex-col gap-2">
-        <h1 className="pt-5 font-extrabold lg:text-3xl">
-          {data.intitule_formation}
-        </h1>
-        <p>{data.nom_of}</p>
-        <div className="flex gap-5 py-2">
+      <div className="mx-auto flex w-[90%] max-w-[1200px] flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <h1 className="pt-5 text-xl font-extrabold lg:text-3xl">
+            {data.intitule_formation}
+          </h1>
+          {data ? (
+            <p className="text-sm font-extralight">
+              {data.nom_of} ASSOCIATION DE GESTION CONSERVATOIRE NATIONAL ARTS
+              METIERS NOUVELLE AQUITAINE
+            </p>
+          ) : null}
+        </div>
+
+        <div className="flex gap-5">
           <Badge className="rounded-md">Eligibles CPF</Badge>
           <Badge className="rounded-md border border-primary bg-background text-primary">
             A distance
           </Badge>
         </div>
-        <p className="flex flex-row items-center text-base">
-          <span className="font-extrabold">
-            <TiPlus />
-          </span>
+        <p className="flex flex-row items-center gap-1 text-base">
+          <TiPlus className="text-2xl font-extrabold" />
           Voir des formations similaires
         </p>
         <Card className="flex flex-col gap-2 border px-5 py-4 shadow-sm">
@@ -49,7 +55,7 @@ function ClientPage(props: Props) {
               <FaEuroSign />
               Prix
             </span>
-            {data.frais_ttc_tot_max}€
+            {data.frais_ttc_tot_max} €
           </p>
           {data.nom_departement && data.code_departement ? (
             <p className="flex flex-col gap-2">
@@ -66,15 +72,19 @@ function ClientPage(props: Props) {
             <p className="text-xl">4,3 ⭐️⭐️⭐️⭐️⭐️ 100 avis{data.avis}</p>
           </Card>
         ) : null}
-        <h4 className="text-lg font-bold">Objectif</h4>
-        <p>{data.objectif_formation}</p>
-        <div className="flex items-center">
-          <FaAngleRight />
-          <p className="text-sm font-bold"> Voir plus </p>
+        <div className="flex flex-col gap-3">
+          <h4 className="text-lg font-bold">Objectif</h4>
+          <p>{data.objectif_formation}</p>
+          <div className="flex items-center">
+            <FaAngleRight />
+            <p className="text-sm font-bold"> Voir plus </p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h4 className="text-lg font-bold">Contenu</h4>
+          <p>{data.contenu_formation}</p>
         </div>
 
-        <h4 className="text-lg font-bold">Contenu</h4>
-        <p>{data.contenu_formation}</p>
         <Card className="flex flex-col gap-3 bg-primary/20 px-5 py-4">
           <h4 className="text-lg font-bold">Points Forts 🔥</h4>
           <p className="">{data.points_forts}</p>
